f6c38ac1396896445909bb506291ff39775ee754
liuxiaobai666 <54610395+liuxiaobai666@users.noreply.github.com>
2023-09-11 15:46:42 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT-LiuXiaoBai
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          爱生活,爱小白.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
