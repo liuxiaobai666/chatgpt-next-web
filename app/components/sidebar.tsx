a60f6cd7a78929c5d6c2a225041242af48732e9d
liuxiaobai666 <54610395+liuxiaobai666@users.noreply.github.com>
2023-09-21 12:39:02 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -174,7 +174,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href="https://www.ounoe.com" target="_blank">
+            <a href="https://www.ounoe.com/archives/378" target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
